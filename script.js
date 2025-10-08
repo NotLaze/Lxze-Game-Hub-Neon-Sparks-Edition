a263a85833ff2c6189d0037e8a28becd18afb7fb
NotLaze <notlazeyt@gmail.com>
2025-10-08 23:00:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 // Spark particle background
 const canvas = document.getElementById("sparkCanvas");
 const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
+let w, h;
 let sparks = [];
+
+function resize() {
+  w = canvas.width = window.innerWidth;
+  h = canvas.height = window.innerHeight;
+}
+window.addEventListener("resize", resize);
+resize();
 
 class Spark {
   constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 2 + 1;
-    this.speedX = Math.random() * 1 - 0.5;
-    this.speedY = Math.random() * 1 - 0.5;
+    this.reset();
+  }
+  reset() {
+    this.x = Math.random() * w;
+    this.y = Math.random() * h;
+    this.vx = (Math.random() - 0.5) * 0.3;
+    this.vy = (Math.random() - 0.5) * 0.3;
+    this.size = Math.random() * 2 + 0.5;
+    this.alpha = Math.random();
+    this.color = `rgba(0,255,255,${this.alpha})`;
   }
   update() {
-    this.x += this.speedX;
-    this.y += this.speedY;
-    if (this.x < 0 || this.x > canvas.width || this.y < 0 || this.y > canvas.height) {
-      this.x = Math.random() * canvas.width;
-      this.y = Math.random() * canvas.height;
-    }
+    this.x += this.vx;
+    this.y += this.vy;
+    this.alpha -= 0.003;
+    if (this.alpha <= 0) this.reset();
   }
   draw() {
-    ctx.fillStyle = "rgba(0,255,255,0.8)";
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
     ctx.fill();
   }
 }
 
+for (let i = 0; i < 150; i++) sparks.push(new Spark());
+
 function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  sparks.forEach(spark => {
-    spark.update();
-    spark.draw();
-  });
+  ctx.clearRect(0, 0, w, h);
+  for (let s of sparks) {
+    s.update();
+    s.draw();
+  }
   requestAnimationFrame(animate);
 }
-
-function init() {
-  for (let i = 0; i < 100; i++) sparks.push(new Spark());
-  animate();
-}
-init();
-
-window.addEventListener("resize", () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-});
+animate();
